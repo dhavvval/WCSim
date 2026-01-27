2f4a2ce673a8ccc5bae1218e0d9f51d7fd52c359
dhavvval <ajana.dhaval.2@gmail.com>
2026-01-27 13:39:09 -0600

    Updates on WCSimTrackInformation & WCSimTrajectory related to DirectParentID

--- include/WCSimTrajectory.hh
+++ include/WCSimTrajectory.hh
@@ -45,6 +45,8 @@ public: // with description
    { return fTrackID; }
    inline G4int GetParentID() const
    { return fParentID; }
+   inline G4int GetDirectParentID() const
+   {return fDirectParentID; }
    inline G4int GetParentPdg() const
    { return fParentPdg; }
    inline G4String GetParticleName() const
@@ -122,6 +124,7 @@ public: // with description
   TrajectoryPointContainer* positionRecord;
   G4int                     fTrackID;
   G4int                     fParentID;
+  G4int                     fDirectParentID;
   G4int                     fParentPdg;
   G4int                     PDGEncoding;
   G4double                  PDGCharge;

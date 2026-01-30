5b1e1949edf5b207d754a303dc82d0f8ecf2b75e
dhavvval <ajana.dhaval.2@gmail.com>
2026-01-30 14:16:59 -0600

    changes in sample readfile

--- include/WCSimRootEvent.hh
+++ include/WCSimRootEvent.hh
@@ -56,7 +56,11 @@ private:
   Int_t fDirectParentID;
 
 public:
-  WCSimRootTrack() {}
+  WCSimRootTrack() : fIpnu(0), fFlag(0), fM(0), fP(0), fE(0), fP2(0), fE2(0), 
+    fStartvol(0), fStopvol(0), fParenttype(0), fTime(0), fTime2(0), fId(0),
+    fTankExitE(0), fPrimaryParentID(-1), fDirectParentID(-1) {
+      for(int i=0; i<3; i++) { fDir[i]=0; fPdir[i]=0; fPdir2[i]=0; fStop[i]=0; fStart[i]=0; fTankExitPos[i]=0; fTankExitMom[i]=0; }
+    }
   WCSimRootTrack(Int_t ipnu, 
 		  Int_t flag, 
 		  Float_t m, 
@@ -113,7 +117,7 @@ public:
   
   void Clear(Option_t *option ="");
 
-  ClassDef(WCSimRootTrack,2)  
+  ClassDef(WCSimRootTrack,3)  
 };
 
 
@@ -148,7 +152,7 @@ private:
   Int_t  fDirectParentID;
 
 public:
-  WCSimRootCherenkovHitTime() {}
+  WCSimRootCherenkovHitTime() : fTruetime(0), fPrimaryParentID(-1), fDirectParentID(-1) {}
   WCSimRootCherenkovHitTime(Float_t truetime,
 			    Int_t   primaryParentID,
 			    Int_t   directParentID);
@@ -158,7 +162,7 @@ public:
   Int_t     GetPrimaryParentID() { return fPrimaryParentID;}
   Int_t     GetDirectParentID() { return fDirectParentID;}
 
-  ClassDef(WCSimRootCherenkovHitTime,2)  
+  ClassDef(WCSimRootCherenkovHitTime,3)  
 };
 
 
